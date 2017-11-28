0094dc4d0ecf3a7ee51c93f6c4df91336c66c071
brian.bi <brian.bun@outlook.com>
2017-11-28 15:10:23 +0800

    init code

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/28
+ * Time: 14:45
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Yaoli_Customcms',
+    __DIR__
+);
